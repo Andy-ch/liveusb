dccc6c596a5659d1ebda3740b41627fa1cc26727
Andrew Che <andy.v.che@gmail.com>
2023-11-22 03:49:53 +0100

    Rework make-iso.sh Resize Vagrant machine disk to 50GB

--- make-iso.sh
+++ make-iso.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -e
 vagrant up
-ansible-playbook -i inventory playbook.yml
-#vagrant halt
-#disk_path=$(VBoxManage showvminfo $(cat .vagrant/machines/default/virtualbox/id) --machinereadable | grep vmdk | sed 's/.*=//g;s/"//g')
-#sudo qemu-nbd -c /dev/nbd0 "${disk_path}"
-#sudo mount /dev/nbd0p1 mnt
-#
-#sudo umount mnt
-#sudo qemu-nbd -d /dev/nbd0
+part_size=$(vagrant ssh -c 'df -h'|grep /dev/sda1|awk '{print $2}')
+sudo modprobe nbd max_part=16
+disk_path=$(VBoxManage showvminfo $(cat .vagrant/machines/default/virtualbox/id) --machinereadable | grep SATA-0-0 | sed 's/.*=//g;s/"//g')
+if [[ "${part_size}" != '46G' ]]
+then
+  vagrant halt
+  sudo qemu-nbd -c /dev/nbd0 "${disk_path}"
+  sudo parted /dev/nbd0 resizepart 1 50GB
+  sudo qemu-nbd -d /dev/nbd0
+  vagrant up
+  echo 'Please unlock the user session manually, and then enter Vault password'
+fi
+say 'Input required' || true
+ansible-playbook --ask-vault-password -i inventory playbook.yml
+vagrant halt
+say 'Input required' || true
+sudo qemu-nbd -c /dev/nbd0 "${disk_path}"
+sudo mount /dev/nbd0p1 mnt
+cp mnt/tmp/andy-liveusb-x86_64.iso ./
+sudo umount mnt
+sudo qemu-nbd -d /dev/nbd0
